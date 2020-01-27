74a82b563d0e8747da12293927117cbc2a9e72ad
Miguel Fontoura <miguel.fontoura1@gmail.com>
2020-01-27 21:04:25 +0000

    MVP block editor done
    still have an issue with materials when saving a new prefab

--- Assets/Scenes_prod/BlockEditor/BlockEditor.cs
+++ Assets/Scenes_prod/BlockEditor/BlockEditor.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace BlockEditor
 {
@@ -10,6 +14,7 @@ namespace BlockEditor
         private const float INCREMENT = 0.50f;
 
         [SerializeField] private GameObject _blockPiecePrefab = null;
+        [SerializeField] private InputField _newBlockNameInputField = null; 
 
         private GridBlock[,] _grid;
 
@@ -41,6 +46,37 @@ namespace BlockEditor
             block.Init(gridBlockX, gridBlockY);
             
             return block;
+        }
+        
+        public void SaveBlock()
+        {
+            var newBlock = new GameObject();
+            newBlock.name = "NewBlock";
+            foreach (var gridBlock in _grid) {
+                if (gridBlock.IsActive()) {
+                    gridBlock.transform.SetParent(newBlock.transform);
+                }
+                else {
+                    StartCoroutine(gridBlock.NiceDestroyCr());
+                }
+            }
+
+            var rb = newBlock.AddComponent<Rigidbody2D>();
+            newBlock.AddComponent<CompositeCollider2D>().geometryType = CompositeCollider2D.GeometryType.Polygons;
+            
+            rb.AddForce(new Vector2(Random.value, 6f), ForceMode2D.Impulse);
+            rb.AddTorque(Random.value, ForceMode2D.Impulse);
+
+            var localPath = "Assets/Blocks/prefab_block_" + _newBlockNameInputField.text + ".prefab";
+            localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+
+            //TODO: we lose the reference to the materials of the grid blocks
+            PrefabUtility.SaveAsPrefabAsset(newBlock.gameObject, localPath);
+        }
+
+        public void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         
     }
--- Assets/Scenes_prod/BlockEditor/GridBlock.cs
+++ Assets/Scenes_prod/BlockEditor/GridBlock.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace BlockEditor
@@ -11,40 +12,70 @@ namespace BlockEditor
         private bool _active;
         private Camera _mainCamera;
         private Collider2D _collider;
+        private SpriteRenderer _spriteRenderer;
 
         private void Awake()
         {
-            _mainCamera = Camera.current;
+            //TODO: optmize
+            _mainCamera = FindObjectOfType<Camera>();
             _collider = GetComponent<Collider2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         public void Init(float gridBlockX, float gridBlockY)
         {
             transform.localPosition = new Vector3(gridBlockX, gridBlockY);
-            GetComponent<SpriteRenderer>().material.color = Color.red;
+            _spriteRenderer.material.color = Color.red;
+            _collider.isTrigger = true;
+
         }
 
-        private void ToggleActive()
+        private void Activate(bool activate)
         {
-            _active = !_active;
+            _active = activate;
+
+            if (activate) {
+                _spriteRenderer.material.color = Color.white;
+                _collider.usedByComposite = true;
+                _spriteRenderer.sortingOrder = 1;
+            }
+            else {
+                _spriteRenderer.material.color = Color.red;
+                _collider.usedByComposite = false;
+                _spriteRenderer.sortingOrder = 0;
+            }
+        }
+        
+        public bool IsActive()
+        {
+            return _active;
         }
 
         private void Update()
         {
-            if (Input.touchCount > 0) {
-                var touch = Input.GetTouch(0);
-                
-                var wp = _mainCamera.ScreenToWorldPoint(Input.GetTouch(0).position);
-                var touchPosition = new Vector2(wp.x, wp.y);
-                
-                 
-                if (_collider == Physics2D.OverlapPoint(touchPosition)){
-                    Debug.Log("HIT!");
-                }
-                else{
-                    Debug.Log("MISS");
-                }
+            if (!Input.GetMouseButtonUp(0)) return;
+            
+            var wp = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            var touchPosition = new Vector2(wp.x, wp.y);
+
+            if (_collider == Physics2D.OverlapPoint(touchPosition)) {
+                Activate(true);
             }
+        }
+
+        public IEnumerator NiceDestroyCr()
+        {
+            yield return new WaitForSeconds(1);
+            while (Math.Abs(_spriteRenderer.color.a) > 0.001f) {
+                _spriteRenderer.color = new Color(
+                    _spriteRenderer.color.r,
+                    _spriteRenderer.color.g,
+                    _spriteRenderer.color.b, 
+                    Mathf.Lerp(_spriteRenderer.color.a, 0, (Time.deltaTime/0.5f)));
+                //_spriteRenderer.color.a.
+                yield return null;
+            }
+            Destroy(gameObject);
         }
     }
 }
