745e7a2cb7c88763d1dfa6e6de43e65a9c104093
Miguel Fontoura <miguel.fontoura1@gmail.com>
2020-01-27 18:27:55 +0000

    started work block editor

---
+++ Assets/Scenes_prod/BlockEditor/BlockEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace BlockEditor
+{
+
+    public class BlockEditor : MonoBehaviour
+    {
+        private const int NUM_GRID_ROWS = 5;
+        private const int NUM_GRID_COLUMNS = 4;
+        private const float INCREMENT = 0.50f;
+
+        [SerializeField] private GameObject _blockPiecePrefab = null;
+
+        private GridBlock[,] _grid;
+
+        private void Awake()
+        {
+            GenerateGrid(NUM_GRID_ROWS, NUM_GRID_COLUMNS);
+        }
+
+        private void GenerateGrid(int rows, int columns)
+        {
+            float xIncrement = 0;
+            float yIncrement = 0;
+
+            _grid = new GridBlock[rows, columns];
+            for (var i = 0; i < rows; i++) {
+                for (var j = 0; j < columns; j++) {
+                    //instantiate preview block
+                    _grid[i,j] = InstantiateBlock(xIncrement, yIncrement);
+                    yIncrement += INCREMENT;
+                }
+                xIncrement += INCREMENT;
+                yIncrement = 0;
+            }
+        }
+
+        private GridBlock InstantiateBlock(float gridBlockX, float gridBlockY)
+        {
+            var block = Instantiate(_blockPiecePrefab, transform).AddComponent<GridBlock>();
+            block.Init(gridBlockX, gridBlockY);
+            
+            return block;
+        }
+        
+    }
+}
---
+++ Assets/Scenes_prod/BlockEditor/GridBlock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace BlockEditor
+{
+    public class GridBlock : MonoBehaviour
+    {
+        public float x;
+        public float y;
+
+        private bool _active;
+        private Camera _mainCamera;
+        private Collider2D _collider;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.current;
+            _collider = GetComponent<Collider2D>();
+        }
+
+        public void Init(float gridBlockX, float gridBlockY)
+        {
+            transform.localPosition = new Vector3(gridBlockX, gridBlockY);
+            GetComponent<SpriteRenderer>().material.color = Color.red;
+        }
+
+        private void ToggleActive()
+        {
+            _active = !_active;
+        }
+
+        private void Update()
+        {
+            if (Input.touchCount > 0) {
+                var touch = Input.GetTouch(0);
+                
+                var wp = _mainCamera.ScreenToWorldPoint(Input.GetTouch(0).position);
+                var touchPosition = new Vector2(wp.x, wp.y);
+                
+                 
+                if (_collider == Physics2D.OverlapPoint(touchPosition)){
+                    Debug.Log("HIT!");
+                }
+                else{
+                    Debug.Log("MISS");
+                }
+            }
+        }
+    }
+}
