d54e38a93f3499f43ba6bd62d59214e20cbcb0a7
Miguel Fontoura <miguel.fontoura1@gmail.com>
2020-01-29 22:35:09 +0000

    Added some art and start of development on of single player mode

---
+++ Assets/LoadUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneManager.LoadSceneAsync("GameUI", LoadSceneMode.Additive);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
