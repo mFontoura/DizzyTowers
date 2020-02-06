254994c0a7ddf11aa6623f39c4589a54d337195e
Miguel Fontoura <miguel.fontoura1@gmail.com>
2020-02-06 18:08:17 +0000

    fixed background draw trigger

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -6,19 +6,29 @@ public class BackgroundManager : MonoBehaviour
     [SerializeField] private FloatVar _towerHeight = null;
     [SerializeField] private Transform _firstBackground = null;
     [SerializeField] private GameObject _backgroundPrefab = null;
+    [SerializeField] private float _cameraSpeed = 1;
     
     private Camera _camera;
     private Transform _cameraTransform;
     private float _backgroundHeight;
     private float backgroundLimit;
+    private float _prevBackPos;
+    
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
         _cameraTransform = _camera.transform;
 
+        _prevBackPos = _firstBackground.position.y;
         _backgroundHeight = _firstBackground.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
-        backgroundLimit = _backgroundHeight / 3;
+        //backgroundLimit = 0f;
+        
+    }
+
+    private void Start()
+    {
+        backgroundLimit += GameManager.Instance.StartingHeight;
     }
 
     private void Update()
@@ -34,11 +44,12 @@ public class BackgroundManager : MonoBehaviour
     
     private void UpdateBackground()
     {
-        if ((_towerHeight.value - GameManager.Instance.StartingHeight) > backgroundLimit) {
-            var newBackground = Instantiate(_backgroundPrefab).GetComponent<Transform>();
-            newBackground.position = new Vector3(_firstBackground.transform.position.x, _towerHeight.value + _backgroundHeight, _firstBackground.transform.position.z);
-            backgroundLimit += _backgroundHeight;
-        }
+        if (!(_towerHeight.value > backgroundLimit)) return;
+        
+        _prevBackPos += _backgroundHeight;
+        var newBackground = Instantiate(_backgroundPrefab).transform;
+        newBackground.position = new Vector3(_firstBackground.transform.position.x, _prevBackPos, _firstBackground.transform.position.z);
+        backgroundLimit += _backgroundHeight;
     }
     
 
@@ -47,7 +58,7 @@ public class BackgroundManager : MonoBehaviour
         var position = _cameraTransform.position;
         position = new Vector3(
             position.x,
-            Mathf.Lerp(position.y, _towerHeight.value > 0 ? _towerHeight.value : 0, Time.deltaTime),
+            Mathf.Lerp(position.y, _towerHeight.value > 0 ? _towerHeight.value : 0, Time.deltaTime * _cameraSpeed),
             position.z);
         _cameraTransform.position = position;
     }
